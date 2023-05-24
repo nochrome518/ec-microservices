17aa892622dbc48bbb7e628f368b0ab9341ada9b
Kiran <kiran@flexm.com>
2023-05-24 19:56:44 +0530

    exception filter & interceptor updated

--- ms-appuser/src/main.ts
+++ ms-appuser/src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { join } from 'path';
 import { AppModule } from './app.module';
 import { Transport } from '@nestjs/microservices'
+import { NotFoundExceptionFilter } from './utilities/filters/not-found.exception-filter';
+import { RequestLoggingInterceptor } from './utilities/interceptors/request-logging.interceptor';
 
 async function bootstrap(){
     const app = await NestFactory.createMicroservice(AppModule, {
@@ -12,7 +14,8 @@ async function bootstrap(){
         protoPath: join(__dirname, 'protos/users.proto'),
         }
     });
-
+    app.useGlobalFilters(new NotFoundExceptionFilter());
+    app.useGlobalInterceptors(new RequestLoggingInterceptor());
     app.listen().then(()=>{
         console.log(`ms-appusers is running`);
     })
